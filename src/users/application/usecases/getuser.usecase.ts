75b5632a5d96965f0af7b9e1cedcfe2c003d7be1
gianniresende <gianniresende@gmail.com.br>
2024-02-25 20:58:33 -0300

    Adding usecase contract

--- src/users/application/usecases/getuser.usecase.ts
+++ src/users/application/usecases/getuser.usecase.ts
@@ -1,12 +1,13 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository'
 import { UserOutput } from '../dtos/user-output'
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
 
 export namespace GetUserUseCase {
   export type Input = { id: string }
 
   export type Output = UserOutput
 
-  export class UseCase {
+  export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(private userRepository: UserRepository.Repository) {}
     async execute(_Input: Input): Promise<Output> {
       const entity = await this.userRepository.findById(_Input.id)
