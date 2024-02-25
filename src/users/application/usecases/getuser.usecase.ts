c5757fd4a73450fbcb14b8631cb020f04901118b
gianniresende <gianniresende@gmail.com.br>
2024-02-25 18:53:16 -0300

    Adding GetUserUseCase

---
+++ src/users/application/usecases/getuser.usecase.ts
@@ -0,0 +1,21 @@
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+
+export namespace GetUserUseCase {
+  export type Input = { id: string }
+
+  export type Output = {
+    id: string
+    name: string
+    email: string
+    password: string
+    createdAt: Date
+  }
+
+  export class UseCase {
+    constructor(private userRepository: UserRepository.Repository) {}
+    async execute(_Input: Input): Promise<Output> {
+      const entity = await this.userRepository.findById(_Input.id)
+      return entity.toJSON()
+    }
+  }
+}
