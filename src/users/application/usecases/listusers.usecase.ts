6e53d7653ef7b5d6b09468da2aeddb8fecc71d96
gianniresende <gianniresende@gmail.com.br>
2024-02-25 21:28:20 -0300

    Adding ListUsersUseCase - part 2

--- src/users/application/usecases/listusers.usecase.ts
+++ src/users/application/usecases/listusers.usecase.ts
@@ -1,18 +1,27 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository'
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
 import { SearchInput } from '@/shared/application/dtos/search-input'
+import { PaginationOutPut, PaginationOutputMapper } from '@/shared/application/dtos/pagination-output'
+import { UserOutput, UserOutputMapper } from '../dtos/user-output'
 
 export namespace ListUsersUseCase {
   export type Input = SearchInput
 
-  export type Output = void
+  export type Output = PaginationOutPut<UserOutput>
 
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(private userRepository: UserRepository.Repository) {}
     async execute(_Input: Input): Promise<Output> {
       const params = new UserRepository.SearchParams(_Input)
       const searchResult = await this.userRepository.search(params)
-      return
+      return this.toOutput(searchResult)
+    }
+
+    private toOutput(searchResult: UserRepository.SearchResult): Output {
+      const items = searchResult.items.map(item => {
+        return UserOutputMapper.toOutput(item)
+      })
+      return PaginationOutputMapper.toOutput(items, searchResult)
     }
   }
 }
