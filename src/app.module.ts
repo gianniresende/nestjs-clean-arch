d74f8580460582bc53fda2f0dcc3294666531f22
gianniresende <gianniresende@gmail.com.br>
2024-02-28 22:13:08 -0300

    Registering SignupUseCase

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,34 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
 import { UsersModule } from './users/infrastructure/users.module'
-
+import { SigninUseCase } from './users/application/usecases/signin.usecase'
+import { UserInMemoryRepository } from './users/infrastructure/database/in-memory/repositories/user-in-memory.repository'
+import { BcryptjsHashProvider } from './users/infrastructure/providers/hash-provider/bcryptjs-hash.provider'
+import { HashProvider } from './shared/application/providers/hash-provider'
+import { UserRepository } from './users/domain/repositories/user.repository'
 @Module({
   imports: [EnvConfigModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'UserRepository',
+      useClass: UserInMemoryRepository,
+    },
+    {
+      provide: 'HashProvider',
+      useClass: BcryptjsHashProvider,
+    },
+    {
+      provide: SigninUseCase.UseCase,
+      useFactory(
+        userRepository: UserRepository.Repository,
+        hashProvider: HashProvider,
+      ) {
+        return new SigninUseCase.UseCase(userRepository, hashProvider)
+      },
+      inject: ['UserRepository', 'HashProvider'],
+    },
+  ],
 })
 export class AppModule {}
