21a0853d414d8a0adbee0d3d2cae1ddb7aa531c9
gianniresende <gianniresende@gmail.com.br>
2024-03-04 20:51:18 -0300

    PrismaUserRepository - Implementing search method - part 4

--- src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int.spec.ts
+++ src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int.spec.ts
@@ -94,5 +94,44 @@ describe('UserModelMapper integration tests', () => {
         expect(`test${index + 1}@email.com`).toBe(item.email)
       })
     })
+
+    it('Should search using filter, sort and paginate', async () => {
+      const createdAt = new Date()
+      const entities: UserEntity[] = []
+      const arrange = ['test', 'a', 'TEST', 'b', 'TeSt']
+      arrange.forEach((element, index) => {
+        entities.push(
+          new UserEntity({
+            ...UserDataBuilder({ name: element }),
+            createdAt: new Date(createdAt.getTime() + index)
+          })
+        )
+      })
+
+      await prismaService.user.createMany({
+        data: entities.map(item => item.toJSON()),
+      })
+
+      const searchOutputPage1 = await sut.search(new UserRepository.SearchParams({
+        page: 1,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'TEST',
+      }))
+
+      expect(searchOutputPage1.items[0].toJSON()).toMatchObject(entities[0].toJSON())
+      expect(searchOutputPage1.items[1].toJSON()).toMatchObject(entities[4].toJSON())
+
+      const searchOutputPage2 = await sut.search(new UserRepository.SearchParams({
+        page: 2,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'TEST',
+      }))
+
+      expect(searchOutputPage2.items[0].toJSON()).toMatchObject(entities[2].toJSON())
+    })
   })
 })
