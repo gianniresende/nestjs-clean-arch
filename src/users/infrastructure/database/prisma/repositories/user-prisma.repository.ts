fbd7c5b76ab2d8d5bd85fc1e2c79cc7bd30f3ca0
gianniresende <gianniresende@gmail.com.br>
2024-03-04 17:11:42 -0300

    PrismaUserRepository - Implementing search method

--- src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
+++ src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
@@ -5,6 +5,8 @@ import { UserRepository } from '@/users/domain/repositories/user.repository'
 import { UserModelMapper } from '../models/user-model.mapper'
 
 export class UserPrismaRepository implements UserRepository.Repository {
+  sortableFields: string[] = ['name', 'createdAt']
+
   constructor(private prismaService: PrismaService) {}
 
   findById(id: string): Promise<UserEntity> {
@@ -22,11 +24,51 @@ export class UserPrismaRepository implements UserRepository.Repository {
   emailExists(email: string): Promise<void> {
     throw new Error('Method not implemented.')
   }
-  sortableFields: string[]
-  search(
+
+  async search(
     props: UserRepository.SearchParams,
   ): Promise<UserRepository.SearchResult> {
-    throw new Error('Method not implemented.')
+    const sortable = this.sortableFields?.includes(props.sort) || false
+    const orderByField = sortable ? props.sort : 'createdAt'
+    const orderByDir = sortable ? props.sortDir : 'desc'
+
+    const count = await this.prismaService.user.count({
+      ...(props.filter && {
+        where: {
+          name: {
+            contains: props.filter,
+            mode: 'insensitive',
+          },
+        },
+      }),
+    })
+
+    const models = this.prismaService.user.findMany({
+      ...(props.filter && {
+        where: {
+          name: {
+            contains: props.filter,
+            mode: 'insensitive',
+          },
+        },
+        orderBy: {
+          [orderByField]: orderByDir,
+        },
+        skip:
+          props.page && props.page > 0 ? (props.page - 1) * props.perPage : 1,
+        take: props.perPage && props.perPage > 0 ? props.perPage : 15,
+      }),
+    })
+
+    return new UserRepository.SearchResult({
+      items: (await models).map(model => UserModelMapper.toEntity(model)),
+      total: count,
+      currentPage: props.page,
+      perPage: props.perPage,
+      sort: orderByField,
+      sortDir: orderByDir,
+      filter: props.filter,
+    })
   }
   async insert(entity: UserEntity): Promise<void> {
     await this.prismaService.user.create({ data: entity.toJSON() })
