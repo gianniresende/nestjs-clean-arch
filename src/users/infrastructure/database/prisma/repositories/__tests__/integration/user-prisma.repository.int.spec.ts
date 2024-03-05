c71de2fd7c69f7ae8820474ecd8cead9c3c84dfd
gianniresende <gianniresende@gmail.com.br>
2024-03-05 19:14:23 -0300

    Integration tests of delete method

--- src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int.spec.ts
+++ src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int.spec.ts
@@ -85,6 +85,28 @@ describe('UserModelMapper integration tests', () => {
     expect(output.name).toBe('new name')
   })
 
+  it('Should throws error on delete when entity not found', async () => {
+    const entity = new UserEntity(UserDataBuilder({}))
+    expect(sut.delete(entity._id)).rejects.toThrow(
+      new NotFoundError(`UserModel not found using ID ${entity._id}`),
+    )
+  })
+
+  it('Should delete a entity', async () => {
+    const entity = new UserEntity(UserDataBuilder({}))
+    const newUser = await prismaService.user.create({
+      data: entity.toJSON(),
+    })
+
+    await sut.delete(entity._id)
+    const output = await prismaService.user.findUnique({
+      where: {
+        id: entity._id,
+      },
+    })
+    expect(output).toBeNull()
+  })
+
   describe('Search method tests', () => {
     it('Should apply only pagination when the other params are null', async () => {
       const createdAt = new Date()
